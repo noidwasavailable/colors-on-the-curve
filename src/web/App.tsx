90ec1c468437184b3d0f274ce8d59f85d073b850
noidwasavailable <26755327+noidwasavailable@users.noreply.github.com>
2026-03-21 16:02:18 +0900

    init: react, biomejs

---
+++ src/web/App.tsx
@@ -0,0 +1,16 @@
+import { APITester } from "./APITester";
+import "./index.css";
+
+export function App() {
+  return (
+    <div className="app">
+      <h1>Hello world</h1>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
+      </p>
+      <APITester />
+    </div>
+  );
+}
+
+export default App;
